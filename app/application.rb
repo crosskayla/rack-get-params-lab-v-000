2e0d92298c14ee24ff725cf378ac322fe198ec44
Kayla Cross <kac288@georgetown.edu>
2018-07-23 21:11:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -43,7 +43,7 @@ class Application
   def handle_add(search_term)
     if @@items.include?(search_term)
       @@cart << search_term
-      return "Added #{search_term}"
+      return "added #{search_term}"
     else
       return "We don't have that item"
     end
