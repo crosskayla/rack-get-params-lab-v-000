b1db8f44d0191924e7bf778f80b828f04e5fcfd0
Kayla Cross <kac288@georgetown.edu>
2018-07-23 21:10:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/add/)
       search_term = req.params["item"]
-      resp.write handle_search(search_term)
+      resp.write handle_add(search_term)
     else
       resp.write "Path Not Found"
     end
